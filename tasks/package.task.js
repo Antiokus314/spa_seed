f0565b9223504d9da781c79d572a7d159b102d47
Antiokus314 <navneetgarg123@gmail.com>
2015-10-09 08:18:56 -0400

    Update package task to include all files in build directory

--- tasks/package.task.js
+++ tasks/package.task.js
@@ -1,7 +1,7 @@
 module.exports = function(gulp, plugins, runSeq) {
   var pkg = require('../package.json');
   gulp.task('package', function(cb) {
-    return gulp.src("build/*")
+    return gulp.src("build/**/*")
       .pipe(plugins.tar([pkg.name, '-', pkg.version, '.tar'].join('')))
       .pipe(plugins.gzip())
       .pipe(gulp.dest('packages'));
