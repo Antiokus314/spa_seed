6b585f51be4ec0a7db4a04f76581e62a1318ff8f
Antiokus314 <navneetgarg123@gmail.com>
2015-11-26 11:52:55 -0500

    Clean up gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -3,25 +3,16 @@ process.env.DEBUG = 'gulp:server'
 
 var fs = require('fs');
 var path = require('path');
-var gulp = require('gulp');
-var plugins = require('gulp-load-plugins')();
-var runSeq = require('run-sequence');
 
 var AppContainer = require('easy-di').create('main', {
   locals: {},
   pkg: require('./package'),
-  gulp: gulp,
-  plugins: plugins,
-  runSeq: runSeq
+  gulp: require('gulp'),
+  plugins: require('gulp-load-plugins')(),
+  runSeq: require('run-sequence')
 });
 
 AppContainer.loadList(require('./config'));
 AppContainer.loadDir(path.resolve(__dirname, 'tasks'));
-
-//fs.readdirSync('./tasks').forEach(function(task) {
-//  if (task.indexOf('.task.js') != -1) {
-//    require(path.join(__dirname, 'tasks', task))(gulp, plugins, runSeq);
-//  }
-//});
 
 gulp.task('default', ['server']);
